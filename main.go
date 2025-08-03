efee7c2f9a42129c9e590ad98db6ccfd7121480a
rcastellanosm <castellanos.r@gmail.com>
2025-08-03 21:01:45 +0200

    more information on tool version

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func main() {
 		Use:   "equationlabs-cli [global options] <command> [command options] [arguments...]",
 		Short: color.GreenString("EquationLabs CLI"),
 		Long: color.GreenString("EquationLabs") + ` CLI version ` + color.YellowString(buildVersion) + ` (c) ` + fmt.Sprintf("(c) 2024-%d Raul Castellanos", time.Now().Year()) + ` (Built on ` + color.YellowString(buildDate) + `)
-Helps developers manage php projects scaffolding from local to production environments and following equationlabs development guidelines (DDD, CQRS).`,
-		Version: fmt.Sprintf("%s (Compiled on %s)", buildVersion, buildDate),
+Creates scaffolded projects following equationlabs development guidelines (DDD, CQRS, Testing, Automation).`,
+		Version: color.GreenString(buildVersion) + ` (c) ` + fmt.Sprintf("2024-%d Raul Castellanos", time.Now().Year()) + ` (On ` + color.YellowString(buildDate) + `)`,
 	}
 
 	// Now we add the commands
