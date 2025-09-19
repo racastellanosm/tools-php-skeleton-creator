d3aa0eeb6ab51ca9680c0d5a46c974e287fb688e
rcastellanosm <castellanos.r@gmail.com>
2025-09-19 20:52:51 +0200

    Add validation for required parameters and new optional parameters also

--- commands/create_symfony_project_command.go
+++ commands/create_symfony_project_command.go
@@ -1,34 +1,48 @@
 package commands
 
 import (
-    "os"
+	"os"
 
-    "github.com/racastellanosm/tools-php-skeleton-creator/utilities"
-    "github.com/racastellanosm/tools-php-skeleton-creator/workflows"
-    "github.com/spf13/cobra"
+	"github.com/racastellanosm/tools-php-skeleton-creator/utilities"
+	"github.com/racastellanosm/tools-php-skeleton-creator/workflows"
+	"github.com/spf13/cobra"
+)
+
+var (
+	projectName string
+	database    string
 )
 
 var CreateProjectWithSymfonyCommand = &cobra.Command{
-    Use:   "create-with-symfony [project-name]",
-    Short: "Create a new PHP project with Symfony Framework",
-    Long: `Create a new PHP project with Symfony framework following recommended development guidelines (DDD, CQRS, Testing, Automation).
+	Use:   "create-with-symfony --name your-project-name [--db postgresql|mysql ]",
+	Short: "Create a new PHP project with Symfony Framework",
+	Long: `Create a new PHP project with Symfony framework following recommended development guidelines (DDD, CQRS, Testing, Automation).
 This command initializes a new project directory with the necessary files and structure for a PHP application.`,
-    Args: cobra.ExactArgs(1),
-    Run: func(cmd *cobra.Command, args []string) {
-        projectName := args[0]
-        workflowDependencies := workflows.WorkflowDependencies{
-            ProjectName: projectName,
-        }
+	Run: func(cmd *cobra.Command, args []string) {
+		workflowDependencies := workflows.WorkflowDependencies{
+			ProjectName: projectName,
+			Database:    database,
+		}
 
-        commandWorkflow := workflows.NewCreateSymfonyProjectWorkflow()
+		commandWorkflow := workflows.NewCreateSymfonyProjectWorkflow()
 
-        if err := commandWorkflow.Handle(workflowDependencies); err != nil {
-            utilities.PrintErrorBox(os.Stdout, `Error: `+err.Error())
-            os.Exit(1)
-        }
+		if err := commandWorkflow.Handle(workflowDependencies); err != nil {
+			utilities.PrintErrorBox(os.Stdout, `Error: `+err.Error())
+			os.Exit(1)
+		}
 
-        dir, _ := os.Getwd()
-        var newVar = "[OK] Project created successfully in " + dir + "/" + projectName
-        utilities.PrintSuccessBox(os.Stdout, newVar)
-    },
+		dir, _ := os.Getwd()
+		var newVar = "[OK] Project created successfully in " + dir + "/" + projectName
+		utilities.PrintSuccessBox(os.Stdout, newVar)
+	},
+}
+
+func init() {
+	CreateProjectWithSymfonyCommand.Flags().StringVar(&projectName, "name", "", "The desired project name")
+	CreateProjectWithSymfonyCommand.Flags().StringVar(&database, "db", "postgresql", "The desired database name (mysql|postgresql)")
+
+	// Required Flags
+	if err := CreateProjectWithSymfonyCommand.MarkFlagRequired("name"); err != nil {
+		utilities.PrintErrorBox(os.Stderr, "Project name is required: ´--name your-project-name´")
+	}
 }
--- workflows/create_symfony_project_workflow.go
+++ workflows/create_symfony_project_workflow.go
@@ -1,68 +1,68 @@
 package workflows
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/racastellanosm/tools-php-skeleton-creator/utilities"
-    create "github.com/racastellanosm/tools-php-skeleton-creator/workflows/create_symfony_project"
-    shared "github.com/racastellanosm/tools-php-skeleton-creator/workflows/shared"
+	"github.com/racastellanosm/tools-php-skeleton-creator/utilities"
+	create "github.com/racastellanosm/tools-php-skeleton-creator/workflows/create_symfony_project"
+	shared "github.com/racastellanosm/tools-php-skeleton-creator/workflows/shared"
 )
 
-type CreteSymfonyProjectWorkflow struct {
-    steps []Step
+type CreateSymfonyProjectWorkflow struct {
+	steps []Step
 }
 
-func (w *CreteSymfonyProjectWorkflow) AddStep(step Step) {
-    w.steps = append(w.steps, step)
+func (w *CreateSymfonyProjectWorkflow) AddStep(step Step) {
+	w.steps = append(w.steps, step)
 }
 
-func NewCreateSymfonyProjectWorkflow() *CreteSymfonyProjectWorkflow {
-    createSymfonyProjetWorkflow := &CreteSymfonyProjectWorkflow{}
+func NewCreateSymfonyProjectWorkflow() *CreateSymfonyProjectWorkflow {
+	createSymfonyProjectWorkflow := &CreateSymfonyProjectWorkflow{}
 
-    createSymfonyProjetWorkflow.AddStep(&create.RequireSymfonySkeletonStep{})
-    createSymfonyProjetWorkflow.AddStep(&create.RequireAdditionalDependenciesStep{})
-    createSymfonyProjetWorkflow.AddStep(&create.RequireDevelopmentDependenciesStep{})
-    createSymfonyProjetWorkflow.AddStep(&create.RewriteYamlConfigToPhpStep{})
-    createSymfonyProjetWorkflow.AddStep(&create.ReorganizeNeededFoldersStep{})
-    createSymfonyProjetWorkflow.AddStep(&shared.AddDockerComposeFileStep{})
-    createSymfonyProjetWorkflow.AddStep(&shared.AddMakefileStep{})
+	createSymfonyProjectWorkflow.AddStep(&create.RequireSymfonySkeletonStep{})
+	createSymfonyProjectWorkflow.AddStep(&create.RequireAdditionalDependenciesStep{})
+	createSymfonyProjectWorkflow.AddStep(&create.RequireDevelopmentDependenciesStep{})
+	createSymfonyProjectWorkflow.AddStep(&create.RewriteYamlConfigToPhpStep{})
+	createSymfonyProjectWorkflow.AddStep(&create.ReorganizeNeededFoldersStep{})
+	createSymfonyProjectWorkflow.AddStep(&shared.AddDockerComposeFileStep{})
+	createSymfonyProjectWorkflow.AddStep(&shared.AddMakefileStep{})
 
-    return createSymfonyProjetWorkflow
+	return createSymfonyProjectWorkflow
 }
 
-func (w *CreteSymfonyProjectWorkflow) Handle(dependencies WorkflowDependencies) error {
-    spinner := utilities.NewSpinner("green")
+func (w *CreateSymfonyProjectWorkflow) Handle(dependencies WorkflowDependencies) error {
+	spinner := utilities.NewSpinner("green")
 
-    for _, step := range w.steps {
-        spinner.Start()
+	for _, step := range w.steps {
+		spinner.Start()
 
-        stepError := step.Execute(dependencies.ProjectName)
-        if stepError != nil {
-            // use rollback step to clean everithing up
-            rollbackError := rollbackInCaseOfFailure(dependencies.ProjectName)
-            if rollbackError != nil {
-                return fmt.Errorf("failed to cleanup workspace for %s : %w", dependencies.ProjectName, rollbackError)
-            }
-            return stepError
-        }
+		stepError := step.Execute(dependencies.ProjectName)
+		if stepError != nil {
+			// use rollback step to clean everything up
+			rollbackError := rollbackInCaseOfFailure(dependencies.ProjectName)
+			if rollbackError != nil {
+				return fmt.Errorf("failed to cleanup workspace for %s : %w", dependencies.ProjectName, rollbackError)
+			}
+			return stepError
+		}
 
-        spinner.Stop()
-    }
+		spinner.Stop()
+	}
 
-    return nil
+	return nil
 }
 
 func rollbackInCaseOfFailure(projectName string) error {
-    fmt.Println("Rollback & Cleaning Everithing Step")
+	fmt.Println("Rollback & Cleaning Everithing Step")
 
-    if err := os.Chdir(".."); err != nil {
-        return fmt.Errorf("failed to change directory to %s: %w", projectName, err)
-    }
+	if err := os.Chdir(".."); err != nil {
+		return fmt.Errorf("failed to change directory to %s: %w", projectName, err)
+	}
 
-    if err := os.RemoveAll(projectName); err != nil {
-        return fmt.Errorf("failed to remove directory %s: %w", projectName, err)
-    }
+	if err := os.RemoveAll(projectName); err != nil {
+		return fmt.Errorf("failed to remove directory %s: %w", projectName, err)
+	}
 
-    return nil
+	return nil
 }
