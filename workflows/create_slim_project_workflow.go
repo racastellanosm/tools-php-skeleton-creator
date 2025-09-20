4f8972612160a90548e6c9701c28b685504c7c77
rcastellanosm <castellanos.r@gmail.com>
2025-09-20 10:39:12 +0200

    add a base workflow to embed from it all the workflow concretions

--- workflows/create_slim_project_workflow.go
+++ workflows/create_slim_project_workflow.go
@@ -1,9 +1,6 @@
 package workflows
 
 import (
-	"fmt"
-
-	"github.com/racastellanosm/tools-php-skeleton-creator/utilities"
 	"github.com/racastellanosm/tools-php-skeleton-creator/workflows/steps"
 	"github.com/racastellanosm/tools-php-skeleton-creator/workflows/steps/shared"
 	"github.com/racastellanosm/tools-php-skeleton-creator/workflows/steps/slim"
@@ -11,14 +8,8 @@ import (
 
 var _ Workflow = (*CreateSlimProjectWorkflow)(nil)
 
-// implement the Workflow interface in this class
-
 type CreateSlimProjectWorkflow struct {
-	steps []steps.Step
-}
-
-func (w *CreateSlimProjectWorkflow) AddStep(step steps.Step) {
-	w.steps = append(w.steps, step)
+	BaseWorkflow
 }
 
 func NewCreateSlimProjectWorkflow() *CreateSlimProjectWorkflow {
@@ -34,23 +25,5 @@ func NewCreateSlimProjectWorkflow() *CreateSlimProjectWorkflow {
 }
 
 func (w *CreateSlimProjectWorkflow) Handle(parameters steps.StepParameters) error {
-	spinner := utilities.NewSpinner("yellow")
-
-	for _, step := range w.steps {
-		spinner.Start()
-
-		stepError := step.Execute(parameters)
-		if stepError != nil {
-			// use rollback step to clean everything up
-			rollbackError := rollbackInCaseOfFailure(parameters.ProjectName)
-			if rollbackError != nil {
-				return fmt.Errorf("failed to cleanup workspace for %s : %w", parameters.ProjectName, rollbackError)
-			}
-			return stepError
-		}
-
-		spinner.Stop()
-	}
-
-	return nil
+	return w.BaseWorkflow.Handle(parameters, "yellow")
 }
--- workflows/create_symfony_project_workflow.go
+++ workflows/create_symfony_project_workflow.go
@@ -1,10 +1,6 @@
 package workflows
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/racastellanosm/tools-php-skeleton-creator/utilities"
 	"github.com/racastellanosm/tools-php-skeleton-creator/workflows/steps"
 	"github.com/racastellanosm/tools-php-skeleton-creator/workflows/steps/shared"
 	"github.com/racastellanosm/tools-php-skeleton-creator/workflows/steps/symfony"
@@ -13,11 +9,7 @@ import (
 var _ Workflow = (*CreateSymfonyProjectWorkflow)(nil)
 
 type CreateSymfonyProjectWorkflow struct {
-	steps []steps.Step
-}
-
-func (w *CreateSymfonyProjectWorkflow) AddStep(step steps.Step) {
-	w.steps = append(w.steps, step)
+	BaseWorkflow
 }
 
 func NewCreateSymfonyProjectWorkflow() *CreateSymfonyProjectWorkflow {
@@ -35,37 +27,5 @@ func NewCreateSymfonyProjectWorkflow() *CreateSymfonyProjectWorkflow {
 }
 
 func (w *CreateSymfonyProjectWorkflow) Handle(parameters steps.StepParameters) error {
-	spinner := utilities.NewSpinner("green")
-
-	for _, step := range w.steps {
-		spinner.Start()
-
-		stepError := step.Execute(parameters)
-		if stepError != nil {
-			// use rollback step to clean everything up
-			rollbackError := rollbackInCaseOfFailure(parameters.ProjectName)
-			if rollbackError != nil {
-				return fmt.Errorf("failed to cleanup workspace for %s : %w", parameters.ProjectName, rollbackError)
-			}
-			return stepError
-		}
-
-		spinner.Stop()
-	}
-
-	return nil
-}
-
-func rollbackInCaseOfFailure(projectName string) error {
-	fmt.Println("Rollback & Cleaning Everything Step")
-
-	if err := os.Chdir(".."); err != nil {
-		return fmt.Errorf("failed to change directory to %s: %w", projectName, err)
-	}
-
-	if err := os.RemoveAll(projectName); err != nil {
-		return fmt.Errorf("failed to remove directory %s: %w", projectName, err)
-	}
-
-	return nil
+	return w.BaseWorkflow.Handle(parameters, "green")
 }
