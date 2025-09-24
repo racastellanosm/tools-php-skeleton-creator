00a57b48f99abca57c4b82799bb990cca1f5439c
rcastellanosm <castellanos.r@gmail.com>
2025-09-24 11:51:43 +0200

    replace placeholder strings with project name utility

--- workflows/steps/symfony/update_path_and_variables_step.go
+++ workflows/steps/symfony/update_path_and_variables_step.go
@@ -30,5 +30,23 @@ func (s *UpdatePathsAndVariablesStep) Execute(parameters steps.StepParameters) e
 		return fmt.Errorf("failed to replace and create file: %w", err)
 	}
 
+	// router file
+	routerFile := parameters.ProjectName + "/config/packages/routing.php"
+	oldString = "'%env(DEFAULT_URI)%'"
+	newString = "'%env(APP_DEFAULT_URI)%'"
+
+	if err := utilities.ReplaceAndCreate(routerFile, oldString, newString); err != nil {
+		return fmt.Errorf("failed to replace and create file: %w", err)
+	}
+
+	// routing file
+	routingFile := parameters.ProjectName + "/config/routes.php"
+	oldString = "''../src/Controller/'"
+	newString = "'../src/UI/Http/Controller/'"
+
+	if err := utilities.ReplaceAndCreate(routingFile, oldString, newString); err != nil {
+		return fmt.Errorf("failed to replace and create file: %w", err)
+	}
+
 	return nil
 }
